9af6c4d905417f91fe7127c912eaff90625db145
Wang Gan <Errorman23@users.noreply.github.com>
2024-10-29 05:40:12 +0800

    Update load_dataset.py

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -8,18 +8,23 @@ def load_dataset(dataset_name, train=True, transform=None):
         transform = transforms.Compose([transforms.ToTensor()])
     
     if dataset_name == 'mnist':
-        dataset = datasets.MNIST(root='/data/mnist', train=train, download=True, transform=transform) # Updated since it was saving the data outside project directory
+        dataset = datasets.MNIST(root='../data/mnist', train=train, download=True, transform=transform)
     elif dataset_name == 'cifar10':
-        dataset = datasets.CIFAR10(root='/data/cifar10', train=train, download=True, transform=transform)
+        dataset = datasets.CIFAR10(root='../data/cifar10', train=train, download=True, transform=transform)
     else:
         raise ValueError("Unsupported dataset")
     
     return dataset
 
 
-def load_test_loader(dataset_name):
+def load_test_loader(dataset_name, num_workers=4, pin_memory=True):
     transform = transforms.Compose([transforms.ToTensor()])
     testset = load_dataset(dataset_name, train=False, transform=transform)
 
-    testloader = DataLoader(testset, batch_size=100, shuffle=False)
-    return testloader
+    return DataLoader(
+        testset,
+        batch_size=128,  # Increase batch size for faster processing
+        shuffle=False,
+        num_workers=num_workers,  # Use multiple workers for data loading
+        pin_memory=pin_memory    # Enable pin memory for faster GPU transfer
+    )

99cebf86c670e1f210bf956acfe5fc28359892bc
Mingtaros <leonardow41@gmail.com>
2024-11-02 10:52:05 +0800

    Add Trained Reference Model

--- src/train_reference_model.py
+++ src/train_reference_model.py
@@ -1,12 +1,15 @@
+import os
+import sys
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-from models.model_mnist import MNISTNet
-from models.model_cifar10 import CIFAR10Net
 from load_dataset import load_dataset
 from evaluate_model import evaluate_model, print_cm
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from models.model_mnist import MNISTNet
+from models.model_cifar10 import CIFAR10Net
 
 
 def train_reference_model(dataset_name):
@@ -15,6 +18,7 @@ def train_reference_model(dataset_name):
     else:
         classification_labels = [str(i) for i in range(10)]
 
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     device = torch.device('mps')
 
     train_batch_size = 64
@@ -27,22 +31,24 @@ def train_reference_model(dataset_name):
     test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
     
     model = MNISTNet() if dataset_name == "mnist" else CIFAR10Net()
+    model = model.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # Training loop
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
-        for batch_idx, (data, target) in enumerate(train_loader):
+        for batch_idx, (inputs, labels) in enumerate(train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
-            output = model(data)
-            loss = criterion(output, target)
+            output = model(inputs)
+            loss = criterion(output, labels)
             loss.backward()
             optimizer.step()
-            
+
             total_loss += loss.item()
-            
+
             if batch_idx % 100 == 0:
                 print(f'Epoch: {epoch+1}/{num_epochs}, Batch: {batch_idx}, Loss: {loss.item()}')
 
@@ -55,12 +61,12 @@ def train_reference_model(dataset_name):
         print()
 
     # save model
-    model_save_path = f"models/reference_{dataset_name}"
+    model_save_path = f"models/reference_{dataset_name}/{dataset_name}_reference.pt"
     torch.save(model.state_dict, model_save_path)
     print(f"Model saved in {model_save_path}")
 
 
 if __name__ == "__main__":
-    dataset_name = "mnist"
-    # dataset_name = "cifar10"
+    # dataset_name = "mnist"
+    dataset_name = "cifar10"
     train_reference_model(dataset_name)
